a63561967e5e15a0fb36780c133b99d18036c276
3vilguy <2122619+3vilguy@users.noreply.github.com>
2019-08-09 01:46:46 +0100

    Trying to add react native ckeditor.

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CKEditor from 'react-native-ckeditor';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <CKEditor
+      style={styles.container}
+      content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque iaculis ex felis. Duis vitae facilisis dui, posuere dapibus ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin odio dolor, pharetra nec eros eget, congue vestibulum nulla. Vestibulum pretium dignissim velit. Cras mattis fermentum velit, et pulvinar arcu molestie sed. Ut vitae lectus quis tellus suscipit rhoncus nec vitae urna. Integer lacinia nisl in tortor vulputate sodales. Cras efficitur accumsan odio luctus eleifend. Nam ut ex quis nulla egestas feugiat. Maecenas placerat sollicitudin posuere. Curabitur dolor nibh, volutpat a porta et, pharetra sodales lectus. Donec dapibus sollicitudin faucibus. Vestibulum placerat vel lacus fermentum consectetur. Phasellus non condimentum sem."}
+      onChange={value => {
+        console.log("Value", value);
+        // setFieldValue('description', value);
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
   },
 });
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rn-editor-test-ckeditor
+
+Demo repo for [react-native-ckeditor](https://github.com/vitalyliber/react-native-ckeditor).
+Uses [Expo](https://expo.io/).
+
+## To get started:
+```
+yarn
+```
+```
+yarn start
+```
+And now use device or emulator to check the app.
